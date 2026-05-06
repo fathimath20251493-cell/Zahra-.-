3d740fba5994b657f4b58bfc21acc64109ba5e23
Zahra1493 <fathimath20251493@miis.ae>
2026-05-06 10:31:49 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,3 +13,5 @@ hours = st.number_input ("Hours Studied", min_value=0.0, step = 0.1)
 if st.button( "Predict Score"):
      predicted_score = model.predict([[hours]])[0]
      st.success (f"Predicted Score: {predicted_score: .2f} ")
+st.write("Sample Training DATA")
+st.dataframe (df)
